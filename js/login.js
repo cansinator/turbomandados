51f9c055880e33f6ba7d7c09d171426d0a433acf
Juan Cansino Martinez <canci@LAPTOP-5BHP5MAT>
2020-08-09 21:29:25 -0500

    TESTfb

--- js/login.js
+++ js/login.js
@@ -1,67 +1,9 @@
-$(document).ready(function () {
-    function statusChangeCallback(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
-        if (response.status === 'connected') {
-            userValidation();
-        } else {
-        }
-    }
-
-    function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
-    }
-
-
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: APIFBID,
-            cookie: true,
-            xfbml: true,
-            version: VERSION
-        });
-
-
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
-    };
-
-    function userValidation() {
-        FB.api('/me', function (response) {
-            let data = {
-                'UX': '',
-                'PX': '',
-                'FB': response.id,
-                'NFB': response.name
-            };
-            consumeServicio('POST', data, CONSULTAUSUARIO, llenaUsuario);
-        });
-    }
-});
-var usr = new Usuarios();
-
 async function validaUsuario(ux, px) {
     let data = {
         'UX': ux,
         'PX': px
     };
     consumeServicio('POST', data, CONSULTAUSUARIO, llenaUsuario);
-}
-
-function statusChangeCallback(response) {
-
-    /* {
-        status: 'connected',
-        authResponse: {
-            accessToken: '...',
-            expiresIn:'...',
-            signedRequest:'...',
-            userID:'...'
-        }
-    } */
 }
 
 function llenaUsuario(usrjson) {
