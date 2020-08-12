b476830c80d95e4826f6a9982ed63a881c74c14c
Juan Cansino Martinez <canci@LAPTOP-5BHP5MAT>
2020-08-11 19:21:36 -0500

    configuracion de sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('js/firebase-app.js')
-importScripts('js/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/4.9.1/firebase-messaging.js')
 importScripts('js/sw-utils.js');
 
 var config = {
